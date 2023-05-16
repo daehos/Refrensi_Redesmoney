4a22b6da589f0c377fe0016bce838f583c3170bc
Muhamad Fiqih Ikhsan <m.fiqihikhsan@gmail.com>
2023-05-16 15:11:51 +0700

    referensi benhard

---
+++ footer.php
@@ -0,0 +1,10 @@
+      <!-- Footer -->
+      <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class=" text-center my-auto">
+            <span>M. Fiqih Ikhsan & Farid Reza Nurhuda
+            </span>
+          </div>
+        </div>
+      </footer>
+      <!-- End of Footer -->
